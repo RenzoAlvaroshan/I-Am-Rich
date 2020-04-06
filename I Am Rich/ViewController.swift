c7e2763ca0d55b979b119f6acd4fe46ed3ec7cae
Renzo Adriano Alvaroshan <renzo.alvaroshan@gmail.com>
2020-04-06 16:44:26 +0700

    Initial Commit

---
+++ I Am Rich/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I Am Rich
+//
+//  Created by Renzo Adriano Alvaroshan on 4/6/20.
+//  Copyright © 2020 Renzo Adriano Alvaroshan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
